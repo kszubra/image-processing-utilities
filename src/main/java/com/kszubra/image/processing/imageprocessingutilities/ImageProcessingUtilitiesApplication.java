634a63916e9d1b4153d5f2645f64878d0c65c707
kacper <kacper.szubra>
2021-05-17 23:55:46 +0200

    First commit

---
+++ src/main/java/com/kszubra/image/processing/imageprocessingutilities/ImageProcessingUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.kszubra.image.processing.imageprocessingutilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingUtilitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageProcessingUtilitiesApplication.class, args);
+    }
+
+}
