79edf9a5904c4d90640a9c1433748e5d6b114a12
kacper <kacper.szubra>
2021-05-18 00:04:15 +0200

    Fast copy as backup

---
+++ src/main/java/com/kszubra/image/processing/imageprocessingutilities/utils/DifferenceColoringUtils.java
@@ -0,0 +1,43 @@
+package com.kszubra.image.processing.imageprocessingutilities.utils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Objects;
+
+public class DifferenceColoringUtils {
+    public void markPixels(BufferedImage base, BufferedImage possiblyChanged, File destination, double tolerance) {
+        if (isImageSizesNotEqual(base, possiblyChanged)) {
+            System.out.println("Not same sizes");
+        } else {
+            int[][] differentPixels = ComparisonUtils.getChangedPixels(base, possiblyChanged, tolerance);
+
+            BufferedImage result = markDifferences(differentPixels, possiblyChanged);
+
+            if (Objects.nonNull(destination)) {
+                ImageComparisonUtil.saveImage(destination, result);
+            }
+
+        }
+    }
+
+    private BufferedImage markDifferences(int[][] differentPixels, BufferedImage possiblyChanged) {
+        BufferedImage result = ImageComparisonUtil.deepCopy(possiblyChanged);
+        Graphics2D graphics = result.createGraphics();
+        graphics.setColor(Color.RED);
+        for (int y = 0; y < possiblyChanged.getHeight(); y++) {
+            for (int x = 0; x < possiblyChanged.getWidth(); x++) {
+                if (differentPixels[y][x] == 1) {
+                    result.setRGB(x, y, Color.RED.getRGB());
+//                    graphics.drawRect(x, y, 100, 100);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isImageSizesNotEqual(BufferedImage expected, BufferedImage actual) {
+        return expected.getHeight() != actual.getHeight() || expected.getWidth() != actual.getWidth();
+    }
+}
---
+++ src/main/java/com/kszubra/image/processing/imageprocessingutilities/utils/ImageComparisonUtils.java
@@ -0,0 +1,81 @@
+package com.kszubra.image.processing.imageprocessingutilities.utils;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class ImageComparisonUtils {
+    public static String getImageCode(BufferedImage image, double toleranceLevel) {
+        StringBuilder sb = new StringBuilder();
+        Integer previousRgb = null;
+        Integer previousColorCode = null;
+        Map<String, Integer> colorCodes = new HashMap<>();
+
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                if(Objects.isNull(previousRgb)) {
+                    previousRgb = image.getRGB(x, y);
+                    previousColorCode = 1;
+                    colorCodes.put(String.valueOf(image.getRGB(x, y)), previousColorCode);
+                    sb.append(previousColorCode);
+                } else {
+                    if (ComparisonUtils.isDifferentPixels(image.getRGB(x, y), previousRgb, toleranceLevel)) {
+                        if(colorCodes.containsKey(String.valueOf(image.getRGB(x,y)))) {
+                            previousRgb = image.getRGB(x,y);
+                            sb.append(previousColorCode);
+
+                        } else {
+                            previousColorCode++;
+                            previousRgb = image.getRGB(x,y);
+                            colorCodes.put(previousRgb.toString(), previousColorCode);
+                            sb.append(colorCodes.get(String.valueOf(image.getRGB(x,y))));
+                            sb.append(previousColorCode);
+                        }
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public static int[][] getChangedPixels(BufferedImage base, BufferedImage possiblyChanged, double toleranceLevel) {
+        long countOfDifferentPixels = 0;
+        int[][] matrix = new int[base.getHeight()][base.getWidth()];
+        for (int y = 0; y < base.getHeight(); y++) {
+            for (int x = 0; x < base.getWidth(); x++) {
+                if (ComparisonUtils.isDifferentPixels(base.getRGB(x, y), possiblyChanged.getRGB(x, y), toleranceLevel)) {
+                    matrix[y][x] = 1;
+                    countOfDifferentPixels++;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static boolean isDifferentPixels(int expectedRgb, int actualRgb, double toleranceLevel) {
+        double differenceConstant = calculateDifferenceConstant(toleranceLevel);
+
+        if (expectedRgb == actualRgb) {
+            return false;
+        } else if (toleranceLevel == 0.0) {
+            return true;
+        }
+
+        int red1 = (expectedRgb >> 16) & 0xff;
+        int green1 = (expectedRgb >> 8) & 0xff;
+        int blue1 = (expectedRgb) & 0xff;
+        int red2 = (actualRgb >> 16) & 0xff;
+        int green2 = (actualRgb >> 8) & 0xff;
+        int blue2 = (actualRgb) & 0xff;
+
+        return (Math.pow(red2 - red1, 2) + Math.pow(green2 - green1, 2) + Math.pow(blue2 - blue1, 2))
+                > differenceConstant;
+    }
+
+    private static double calculateDifferenceConstant(double toleranceLevel) {
+        return Math.pow(toleranceLevel * Math.sqrt(Math.pow(255, 2) * 3), 2);
+    }
+}
